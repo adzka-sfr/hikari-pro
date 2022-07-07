e31b68f3a429d4b15a880ae01ea29e2fde85a171
Adzka sfr <adzka.sfr@gmail.com>
2022-07-07 07:25:30 +0700

    menambah code pembaca file pdf

--- app/superuser/employee_management/index/_help/index.php
+++ app/superuser/employee_management/index/_help/index.php
@@ -128,31 +128,10 @@ include('../../app_name.php') ?>
 
                   <div class="row">
                     <div class="col-md-12">
-                      <h2><b>How to use</b></h2>
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
-                      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quia at eaque officiis nobis. Minus veniam saepe, tempora quam quis dolores dolore dolorem numquam porro reiciendis perferendis odio esse assumenda.
+                      <!-- <h2><b>How to use</b></h2> -->
+                      <object data="guide.pdf" type="application/pdf" width="100%" height="800"></object>
                     </div>
                   </div>
-
-                  <div class="row">
-                    <div class="col-md-12">
-
-                      <h2><b>How it works</b></h2>
-                      <ul>
-                        <li>Department</li>
-                        <li>Section</li>
-                        <li>Group</li>
-                      </ul>
-                    </div>
-                  </div>
-
                 </div>
               </div>
             </div>
