8f576bc297392416de746a7cc586e1ab2be6fd7c
Adzka SFR <adzka.sfr@gmail.com>
2022-09-15 13:22:16 +0700

    mengahapus contoh

--- app/production/form_ng/p/index/ng/form2.php
+++ app/production/form_ng/p/index/ng/form2.php
@@ -15,7 +15,6 @@
     <div class="row">
         <div class="col-md-6">
             <!-- isi gambar -->
-            <!-- cek -->
             <!-- gambar 1 -->
             <div class="row">
                 <div class="col-md-12">
