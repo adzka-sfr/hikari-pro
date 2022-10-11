f9a95831219eb0f881936358c5ccc0ed0329df17
Adzka SFR <adzka.sfr@gmail.com>
2022-10-11 14:58:26 +0700

    menambah gambar dan melakukan hover gambar menjadi putih

--- dashboard/dashboard.php
+++ dashboard/dashboard.php
@@ -62,19 +62,31 @@
             if (!empty($r_app_pc)) {
               // ambil semua data previlege
               $q_app_pre = mysqli_query($connect, "SELECT * from t_previlege WHERE c_id = '$_SESSION[id]' ORDER BY c_name");
+              $prev = 10;
               while ($d_app_pre = mysqli_fetch_array($q_app_pre)) {
+
                 $d_dir_pre = $d_app_pre['c_dir'];
                 $d_name_pre = $d_app_pre['c_name'];
                 $d_img_pre = $d_app_pre['c_img'];
             ?>
                 <a href="<?= base_url('app/' . $d_dir_pre) ?>">
-                  <li class="zoom">
-                    <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_pre . '.png') ?>" alt="<?= $d_name_pre ?>" height="50" width="50"></span>
+                  <li class="zoom" onmouseover="mouseOver<?= $prev ?>()" onmouseout="mouseOut<?= $prev ?>()">
+                    <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_pre . '.png') ?>" id="prev<?= $prev ?>" alt="<?= $d_name_pre ?>" height="50" width="50"></span>
                     <span class="glyphicon-class"><?= $d_name_pre ?></span>
                   </li>
+                  <script type="text/javascript">
+                    function mouseOver<?= $prev ?>() {
+                      document.getElementById("prev<?= $prev ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_pre . '_w.png') ?>";
+                    }
+
+                    function mouseOut<?= $prev ?>() {
+                      document.getElementById("prev<?= $prev ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_pre . '.png') ?>"
+                    }
+                  </script>
                 </a>
 
               <?php
+                $prev++;
               }
             } else {
               // jika tidak ada previlege
@@ -107,6 +119,7 @@
             <!-- isi konten aplikasinya -->
             <?php
             $q_app_m = mysqli_query($connect, "SELECT * from t_app WHERE c_group = 'managerial' ORDER BY c_dir");
+            $manag = 100;
             while ($d_app_m = mysqli_fetch_array($q_app_m)) {
               $d_dir_m = $d_app_m['c_dir'];
               $d_name_m = $d_app_m['c_name'];
@@ -114,13 +127,23 @@
             ?>
 
               <a href="<?= base_url('app/managerial/' . $d_dir_m) ?>">
-                <li class="zoom">
-                  <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_m . '.png') ?>" alt="<?= $d_name_m ?>" height="50" width="50"></span>
+                <li class="zoom" onmouseover="mouseOver<?= $manag ?>()" onmouseout="mouseOut<?= $manag ?>()">
+                  <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_m . '.png') ?>" id="manag<?= $manag ?>" alt="<?= $d_name_m ?>" height="50" width="50"></span>
                   <span class="glyphicon-class"><?= $d_name_m ?></span>
                 </li>
+                <script type="text/javascript">
+                  function mouseOver<?= $manag ?>() {
+                    document.getElementById("manag<?= $manag ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_m . '_w.png') ?>";
+                  }
+
+                  function mouseOut<?= $manag ?>() {
+                    document.getElementById("manag<?= $manag ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_m . '.png') ?>"
+                  }
+                </script>
               </a>
 
             <?php
+              $manag++;
             }
             ?>
 
@@ -142,6 +165,7 @@
             <!-- isi konten aplikasinya -->
             <?php
             $q_app_p = mysqli_query($connect, "SELECT * from t_app WHERE c_group = 'production' ORDER BY c_name");
+            $prod = 500;
             while ($d_app_p = mysqli_fetch_array($q_app_p)) {
               $d_dir_p = $d_app_p['c_dir'];
               $d_name_p = $d_app_p['c_name'];
@@ -149,13 +173,23 @@
             ?>
 
               <a href="<?= base_url('app/production/' . $d_dir_p) ?>">
-                <li class="zoom">
-                  <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_p . '.png') ?>" alt="<?= $d_name_p ?>" height="50" width="50"></span>
+                <li class="zoom" onmouseover="mouseOver<?= $prod ?>()" onmouseout="mouseOut<?= $prod ?>()">
+                  <span class="glyphicon " aria-hidden="true"><img src="<?= base_url('_assets/production/icons/projects/' . $d_img_p . '.png') ?>" id="prod<?= $prod ?>" alt="<?= $d_name_p ?>" height="50" width="50"></span>
                   <span class="glyphicon-class"><?= $d_name_p ?></span>
                 </li>
+                <script type="text/javascript">
+                  function mouseOver<?= $prod ?>() {
+                    document.getElementById("prod<?= $prod ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_p . '_w.png') ?>";
+                  }
+
+                  function mouseOut<?= $prod ?>() {
+                    document.getElementById("prod<?= $prod ?>").src = "<?= base_url('_assets/production/icons/projects/' . $d_img_p . '.png') ?>"
+                  }
+                </script>
               </a>
 
             <?php
+              $prod++;
             }
             ?>
 
