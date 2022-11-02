a2c09d77c427b5b349526470e82c89d163cc1f00
Adzka SFR <adzka.sfr@gmail.com>
2022-11-02 07:32:08 +0700

    menambah data back end jika didapatkan page nya

--- app/production/mezzanine/p/index/usa/data/backend.php
+++ app/production/mezzanine/p/index/usa/data/backend.php
@@ -3,3 +3,11 @@ include '../koneksi.php';
 $datashowtime = date('D, d M Y H:i:s');
 include 'DataHours2.php';
 include 'DataHours16.php';
+
+if ($_GET['p'] == 'csn') {
+    // include "createslip.php";
+} elseif ($_GET['p'] == 'kon') {
+    // include "create_slip/create_slip2.php";
+} else {
+    // include "create_slip/create_slip.php";
+}
