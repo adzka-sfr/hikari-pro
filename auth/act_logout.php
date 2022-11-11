ab1a87764d3189a1dc8b7e3a56a000b3e0da7d48
Adzka SFR <adzka.sfr@gmail.com>
2022-11-11 07:21:16 +0700

    menambah log login dan logout

--- auth/act_logout.php
+++ auth/act_logout.php
@@ -1,5 +1,28 @@
 <?php
 require_once "../_config/koneksi.php";
 
+// log activity
+$l_t = $now;
+$sy_n = "Hikari";
+$p_n = "Logout";
+$q = "select";
+$e_n = $_SESSION['nama'];
+$e_i = $_SESSION['id'];
+$c_i = $_SERVER['REMOTE_ADDR'];
+$c_n = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+$s_n = $_SERVER['SCRIPT_NAME'];
+$h = $_SERVER['HTTP_HOST'];
+mysqli_query($connect_log, "INSERT INTO activity_log set
+                                    log_time = '$l_t',
+                                    system_name = '$sy_n',
+                                    process_name = '$p_n',
+                                    query = '$q',
+                                    employee_name = '$e_n',
+                                    employee_id = '$e_i',
+                                    computer_ip = '$c_i',
+                                    computer_name = '$c_n',
+                                    script_name = '$s_n',
+                                    host = '$h'");
+
 session_destroy();
 echo "<script>window.location='" . base_url('auth') . "';</script>";
--- auth/authen.php
+++ auth/authen.php
@@ -17,7 +17,29 @@ if (!empty($row)) {
     $_SESSION['dept'] = $row['dept'];
     $_SESSION['jabatan'] = $row['jabatan'];
     echo "oke";
-} else {
 
+    // log activity
+    $l_t = $now;
+    $sy_n = "Hikari";
+    $p_n = "Login";
+    $q = "select";
+    $e_n = $_SESSION['nama'];
+    $e_i = $_SESSION['id'];
+    $c_i = $_SERVER['REMOTE_ADDR'];
+    $c_n = gethostbyaddr($_SERVER['REMOTE_ADDR']);
+    $s_n = $_SERVER['SCRIPT_NAME'];
+    $h = $_SERVER['HTTP_HOST'];
+    mysqli_query($connect_log, "INSERT INTO activity_log set
+                                    log_time = '$l_t',
+                                    system_name = '$sy_n',
+                                    process_name = '$p_n',
+                                    query = '$q',
+                                    employee_name = '$e_n',
+                                    employee_id = '$e_i',
+                                    computer_ip = '$c_i',
+                                    computer_name = '$c_n',
+                                    script_name = '$s_n',
+                                    host = '$h'");
+} else {
     echo "error";
 }
