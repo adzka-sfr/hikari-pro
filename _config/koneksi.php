eeb8bb7d4a191e56db7204ea996698eaf5f579f6
Adzka SFR <adzka.sfr@gmail.com>
2022-11-11 07:20:29 +0700

    menambah database lain untuk kebutuhan project dan log

--- _config/koneksi.php
+++ _config/koneksi.php
@@ -1,15 +1,24 @@
 <?php
 // setting default timezone
 date_default_timezone_set('Asia/Jakarta');
+$now = date('Y-m-d H:i:s');
 session_start();
 
 $servername = "localhost";
 $username = "root";
 $password = "";
-$db = "hikari";
 
-// Create connection
-$connect = new mysqli($servername, $username, $password, $db);
+// database
+$db1 = "hikari";
+$db2 = "hikari_project";
+$db3 = "hikari_log";
+
+// Create connection for main database (hikari)
+$connect = new mysqli($servername, $username, $password, $db1);
+// Create connection for project database (hikari_project)
+$connect_pro = new mysqli($servername, $username, $password, $db2);
+// Create connection for log database (hikari_log)
+$connect_log = new mysqli($servername, $username, $password, $db3);
 
 // Check connection
 if ($connect->connect_error) {
