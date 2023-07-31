b453d83c8e5302ea9341402e6e2adcf6c8e2b39e
Adzka sfr <adzka.sfr@gmail.com>
2023-07-31 14:52:32 +0700

    add disconnect handler
    outside 1 all

--- app/production/finalcheck/p/kfwqptg/outside1/pagedata.php
+++ app/production/finalcheck/p/kfwqptg/outside1/pagedata.php
@@ -19,11 +19,65 @@ $data = mysqli_fetch_array($sql);
 <script src="../source/dropdown_search/jquery-3.4.1.js" crossorigin="anonymous"></script>
 <script src="../source/dropdown_search/select2.min.js"></script>
 <script src="../source/sweetalert2/dist/sweetalert2.all.min.js"></script>
+<script src="<?= base_url('_bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
 <!-- judul pagedata -->
 <hr>
 <h4><i class="fa fa-pencil-square-o"></i> <u>Check Card - Completeness</u></h4>
 <!-- judul pagedata -->
+
+<!-- modal untuk cek koneksi -->
+<div class="modal fade" id="lostmodal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="col-12 modal-title fs-5 text-center" style="color: red;" id="staticBackdropLabel"><img src="<?= base_url('_assets/production/gif/heart.gif') ?>" width="100px"> Koneksi Terputus! <img src="<?= base_url('_assets/production/gif/heart.gif') ?>" width="100px"></h1>
+            </div>
+            <div class="modal-body" style="font-size: 15px; ">
+                Yang harus dilakukan:
+                <ol>
+                    <li>Pastikan Wifi pada Tab menyala (berwana biru)</li>
+                    <li>Pastikan Wifi terhubung dengan jaringan Yijak-Prolt3a</li>
+                    <li>Tunggu hingga jaringan kembali stabil</li>
+                    <li>Jika sudah terhubung kembali, ulangi kegiatan terakhir anda pada sistem</li>
+                </ol>
+                Silahkan menghubungi ICTM jika poin 1-3 sudah dilakukan namun tidak kunjung tersambung
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" style="display: none;" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
+                <span class="col-12 text-center">
+                    <button type="button" disabled class="btn btn-primary">Mencoba terubung kembali...</button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function calltry() {
+        var check_con = "connect";
+        $.ajax({
+            url: '../source/connection_check.php',
+            type: 'POST',
+            data: {
+                "check_con": check_con
+            },
+            success: function(response) {
+                successconnection();
+            },
+        });
+    }
+
+    function lostconnection() {
+        $('#lostmodal').modal('toggle');
+        setInterval(calltry, 1000);
+    }
+
+    function successconnection() {
+        clearInterval();
+        $('#lostmodal').modal('hide');
+    }
+</script>
+<!-- modal untuk cek koneksi -->
 
 <!-- judul -->
 <table class="table table-bordered" style="font-size: large;">
@@ -130,6 +184,9 @@ $data = mysqli_fetch_array($sql);
                     },
                     success: function(response) {
                         console.log(response);
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
             }
@@ -170,6 +227,9 @@ $data = mysqli_fetch_array($sql);
                 success: function(response) {
                     var response = JSON.parse(response);
                     $('#note1').html(response.note1);
+                },
+                error: function() {
+                    lostconnection()
                 }
             });
         })
@@ -189,6 +249,9 @@ $data = mysqli_fetch_array($sql);
                 success: function(response) {
                     var response = JSON.parse(response);
                     $('#note1').html(response.note1);
+                },
+                error: function() {
+                    lostconnection()
                 }
             });
         })
@@ -273,6 +336,9 @@ $data = mysqli_fetch_array($sql);
                                     $('#pagedata').html(data);
                                     window.scrollTo(0, 100);
                                     return false;
+                                },
+                                error: function() {
+                                    lostconnection()
                                 }
                             });
                         } else if (response.status == 'NOT-YET') {
@@ -303,6 +369,9 @@ $data = mysqli_fetch_array($sql);
                                             $('#pagedata').html(data);
                                             window.scrollTo(0, 100);
                                             return false;
+                                        },
+                                        error: function() {
+                                            lostconnection()
                                         }
                                     });
                                 }
@@ -316,6 +385,9 @@ $data = mysqli_fetch_array($sql);
                                 timer: 2000
                             })
                         }
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
 
--- app/production/finalcheck/p/kfwqptg/outside1/pagedata2.php
+++ app/production/finalcheck/p/kfwqptg/outside1/pagedata2.php
@@ -33,6 +33,59 @@ if ($c_code_type == 'f') {
 <h4><i class="fa fa-pencil-square-o"></i> <u>Check Card - Outside</u></h4>
 <!-- judul pagedata -->
 
+<!-- modal untuk cek koneksi -->
+<div class="modal fade" id="lostmodal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="col-12 modal-title fs-5 text-center" style="color: red;" id="staticBackdropLabel"><img src="<?= base_url('_assets/production/gif/heart.gif') ?>" width="100px"> Koneksi Terputus! <img src="<?= base_url('_assets/production/gif/heart.gif') ?>" width="100px"></h1>
+            </div>
+            <div class="modal-body" style="font-size: 15px; ">
+                Yang harus dilakukan:
+                <ol>
+                    <li>Pastikan Wifi pada Tab menyala (berwana biru)</li>
+                    <li>Pastikan Wifi terhubung dengan jaringan Yijak-Prolt3a</li>
+                    <li>Tunggu hingga jaringan kembali stabil</li>
+                    <li>Jika sudah terhubung kembali, ulangi kegiatan terakhir anda pada sistem</li>
+                </ol>
+                Silahkan menghubungi ICTM jika poin 1-3 sudah dilakukan namun tidak kunjung tersambung
+            </div>
+            <div class="modal-footer">
+                <!-- <button type="button" style="display: none;" class="btn btn-secondary" data-bs-dismiss="modal">Close</button> -->
+                <span class="col-12 text-center">
+                    <button type="button" disabled class="btn btn-primary">Mencoba terubung kembali...</button>
+                </span>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function calltry() {
+        var check_con = "connect";
+        $.ajax({
+            url: '../source/connection_check.php',
+            type: 'POST',
+            data: {
+                "check_con": check_con
+            },
+            success: function(response) {
+                successconnection();
+            },
+        });
+    }
+
+    function lostconnection() {
+        $('#lostmodal').modal('toggle');
+        setInterval(calltry, 1000);
+    }
+
+    function successconnection() {
+        clearInterval();
+        $('#lostmodal').modal('hide');
+    }
+</script>
+<!-- modal untuk cek koneksi -->
+
 <!-- judul -->
 <table class="table table-bordered" style="font-size: large;">
     <thead>
@@ -451,6 +504,9 @@ if ($c_code_type == 'f') {
                         var data = JSON.parse(data);
                         // $('#processeditng').val(data.process);
                         $('#codetypeeditng').val(data.codetype);
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
                 // untuk mengisi value select cabinet
@@ -469,6 +525,9 @@ if ($c_code_type == 'f') {
                             $(this).val(data.cabinet).trigger('change');
                             //         $('#supplier').val(null).trigger('change.select2');
                         });
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
 
@@ -489,6 +548,9 @@ if ($c_code_type == 'f') {
                             // do something with `item`
                             $('#' + item).prop('checked', true);
                         });
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
             }
@@ -594,6 +656,9 @@ if ($c_code_type == 'f') {
                 success: function(response) {
                     var response = JSON.parse(response);
                     $('#note1').html(response.note1);
+                },
+                error: function() {
+                    lostconnection()
                 }
             });
         })
@@ -614,6 +679,9 @@ if ($c_code_type == 'f') {
                 success: function(response) {
                     var response = JSON.parse(response);
                     $('#note1').html(response.note1);
+                },
+                error: function() {
+                    lostconnection()
                 }
             });
         })
@@ -693,6 +761,9 @@ if ($c_code_type == 'f') {
                         $('#pagedata').html(data);
                         window.scrollTo(0, 100);
                         return false;
+                    },
+                    error: function() {
+                        lostconnection()
                     }
                 });
             })
@@ -742,6 +813,9 @@ if ($c_code_type == 'f') {
 
                                     });
                                 }
+                            },
+                            error: function() {
+                                lostconnection()
                             }
                         });
                     }
